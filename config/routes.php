eddf18112871503855ab776d809130c953cc33cd
Rafael N. Garbinatto <rafaelgarbinatto@hotmail.com.br>
2017-07-17 23:05:07 -0300

    Modify nameSpace
    - Modified old project namespace to example

--- config/routes.php
+++ config/routes.php
@@ -2,13 +2,13 @@
 // Routes
 
 //Account # START
-$app->get('/api/account/create', 'App\Billing\Controller\AccountController:createAction');
-$app->get('/api/account/update', 'App\Billing\Controller\AccountController:updateAction');
-$app->get('/api/account/list', 'App\Billing\Controller\AccountController:fetchAllAction');
+$app->get('/api/account/create', 'App\Example\Controller\AccountController:createAction');
+$app->get('/api/account/update', 'App\Example\Controller\AccountController:updateAction');
+$app->get('/api/account/list', 'App\Example\Controller\AccountController:fetchAllAction');
 //Account # END
 
 //CONTRACT # START
-$app->get('/api/contract/create', 'App\Billing\Controller\ContractController:createAction');
-$app->get('/api/contract/update', 'App\Billing\Controller\ContractController:updateAction');
-$app->get('/api/contract/list', 'App\Billing\Controller\ContractController:fetchAllAction');
+$app->get('/api/contract/create', 'App\Example\Controller\ContractController:createAction');
+$app->get('/api/contract/update', 'App\Example\Controller\ContractController:updateAction');
+$app->get('/api/contract/list', 'App\Example\Controller\ContractController:fetchAllAction');
 //CONTRACT # END
