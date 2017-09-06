02d90c0856d47a07e54e0aac0f74d2ab15d2db9a
Marcos Evaldt <marcosevaldt@gmail.com>
2017-09-06 11:05:25 -0300

    Update AccountController.php

--- src/App/Example/Controller/AccountController.php
+++ src/App/Example/Controller/AccountController.php
@@ -7,10 +7,4 @@ use App\Core\Controller\AbstractController;
 class AccountController extends AbstractController
 {
 
-    public function __construct($container)
-    {
-        parent::__construct($container);
-        $this->modelEntity = new AccountModel($container->get('em'));
-    }
-
 }
