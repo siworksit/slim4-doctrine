a7ea0b7dc3732bbab47d28ea5beb9acfcc36e5c0
Rafael N. Garbinatto <rafaelgarbinatto@gmail.com>
2021-05-01 04:05:46 -0300

    # composer update slim4

--- src/settings.php
+++ src/settings.php
@@ -3,11 +3,16 @@ return [
     'settings' => [
         'displayErrorDetails' => true, // set to false in production
         'addContentLengthHeader' => false, // Allow the web server to send the content-length header
-
         'doctrine' => [
             'cache_dir' => __DIR__ . '../var/doctrine',
-            'metadata_dirs' => [
-                __DIR__ . '/Doctrine/Example/Entity'
+            'meta' => [
+                'entity_path' => [
+                    __DIR__ . '/Doctrine/Example/Entity'
+                ],
+                'auto_generate_proxies' => true,
+                'proxy_dir' => __DIR__ . '/Doctrine/Example/Entity',
+                'cache' => null,
+                'simple_annotation_reader' => false,
             ],
             'auto_generate_proxies' => true,
             // if true, metadata caching is forcefully disabled
